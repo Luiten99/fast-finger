fd5152353c2da1cff6b8b0b7b805bb2a20b77564
tenjodiego961 <79155327+tenjodiego961@users.noreply.github.com>
2021-06-10 15:35:17 -0500

    add html and css

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import validateWords from './plugins/validateWords';
+
+const classValidate: validateWords = new validateWords;
---
+++ src/plugins/validateWords.ts
@@ -0,0 +1,8 @@
+class ValidateWords {
+    private inputWords: HTMLInputElement;
+    constructor() {
+        this.inputWords = document.querySelector('.container-words__input');
+    }
+}
+
+export default ValidateWords
